ed54ac0ce46838fcd67eeaef85cd3349aefb7bcc
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2025-01-06 15:57:55 +0530

    feat:fix the issue with neon postgressql

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { Alert, Image, Linking, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ReactNativeModal } from "react-native-modal";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -65,6 +66,14 @@ const SignUp = () => {
       // and redirect the user
       if (signUpAttempt.status === "complete") {
         // TODO create a databse user
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: signUpAttempt.createdUserId,
+          }),
+        });
 
         await setActive({ session: signUpAttempt.createdSessionId });
         setverification({
