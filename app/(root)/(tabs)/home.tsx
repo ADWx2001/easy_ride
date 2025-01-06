7b5a2d62e4db52564e0c4c92c10acae39b81ff08
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2025-01-06 13:49:14 +0530

    feat: fixed navigation padding issue

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -15,12 +15,13 @@
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   const { user } = useUser();
 
   return (
-    <View>
+    <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
@@ -32,6 +33,6 @@ export default function Home() {
           <Text>Sign up</Text>
         </Link>
       </SignedOut>
-    </View>
+    </SafeAreaView>
   );
 }
